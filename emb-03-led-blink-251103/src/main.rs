04b92f843714dd41296d1f418ecc154189f25790
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-03 21:07:03 +0100

    Finish Led Blink

--- emb-03-led-blink-251103/src/main.rs
+++ emb-03-led-blink-251103/src/main.rs
@@ -2,25 +2,37 @@
 #![no_main]
 #![no_std]
 
-// No std means there is no standard libarary
-// This means no Vec, no String, no println!() and so on
-
-// No main means we do not emit a main symbol from the program
-// To define the entry point of the program we import the #[entry] macro from cortex_m_rt crate
-// Note that the function is still called main() but could be anything
-// Also note that we return ! from it, which means that our program never terminates, it will always keep running
+// Basic Commands
+// Build:       cargo build
+// Inspect:     cargo readobj --target thumbv7em-none-eabihf --bin led-blink -- --file-header
+// Flash:       cargo embed
+// New Terminal
+// Open GDB:    arm-none-eabi-gdb ./target/thumbv7em-none-eabihf/debug/led-blink
+// Connect:     target remote :1337
 
 use cortex_m_rt::entry;
 use embedded_hal::digital::OutputPin;
-use microbit::board::Board;
+use embedded_hal::delay::DelayNs;
+use microbit::{board::Board, hal::Timer};
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
     let mut board = Board::take().unwrap();
-    
-    board.display_pins.col1.set_low().unwrap();
+    // This creates a new timer instance somehow related to Timer0
+    // This seems to be one of many different timers in this microcontroller
+    let mut timer = Timer::new(board.TIMER0);
+
+    // To light up an led you need to set its col pin to low and its row pin to high
     board.display_pins.row1.set_high().unwrap();
 
-    loop {}
+    loop {
+        board.display_pins.col1.set_low().unwrap();
+        board.display_pins.col2.set_high().unwrap();
+        timer.delay_ms(500);
+
+        board.display_pins.col1.set_high().unwrap();
+        board.display_pins.col2.set_low().unwrap();
+        timer.delay_ms(500);
+    }
 }
