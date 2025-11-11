d366949025eae0b5eb34afc1801c61f834a0f56c
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-11 21:03:16 +0100

    Add Project 08

---
+++ emb-08-led-hal-251111/src/main.rs
@@ -0,0 +1,24 @@
+
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use nrf52833_hal::{gpio, pac};
+use embedded_hal::digital::OutputPin;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+    let p0 = gpio::p0::Parts::new(peripherals.P0);
+    let _row1 = p0.p0_21.into_push_pull_output(gpio::Level::High);
+    let _col1 = p0.p0_28.into_push_pull_output(gpio::Level::Low);
+
+    let _col3 = p0.p0_31.into_push_pull_output(gpio::Level::Low);
+    let mut row3 = p0.p0_15.into_push_pull_output(gpio::Level::High);
+
+    loop{
+        row3.set_high().unwrap();
+        row3.set_low().unwrap();
+    }
+}
