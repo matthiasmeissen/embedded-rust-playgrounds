1ff751a3bba4e44c1cccbd039c937675180e641f
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-29 15:32:14 +0100

    Finish Register Typesafe

--- emb-22-register-typesafe-251129/src/main.rs
+++ emb-22-register-typesafe-251129/src/main.rs
@@ -1,6 +1,20 @@
 #![no_main]
 #![no_std]
 
+
+// Basic Commands
+// Build:           cargo build
+// Inspect:         cargo readobj --target thumbv7em-none-eabihf --bin registers -- --file-header
+// Flash:           cargo embed
+
+// New Terminal
+// Open GDB:        arm-none-eabi-gdb ./target/thumbv7em-none-eabihf/debug/registers
+// Connect:         target remote :1337
+// Set Mem:         set mem inaccessible-by-default off
+// Add Breakpoint:  break main.rs:27
+// Continue:        continue
+
+
 #[allow(unused_imports)]
 use registers::entry;
 
