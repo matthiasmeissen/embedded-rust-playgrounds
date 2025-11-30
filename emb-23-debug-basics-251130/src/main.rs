2fd54f9137149a8c5c88af41ec49321eec30d9b5
matthiasmeissen <meissenmatthias@gmail.com>
2025-11-30 14:14:25 +0100

    Add Project 23

---
+++ emb-23-debug-basics-251130/src/main.rs
@@ -0,0 +1,25 @@
+
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board, gpio::DisplayPins, hal::Timer};
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let board = board::Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut pins = board.display_pins;
+
+    pins.row1.set_high().unwrap();
+    pins.col1.set_low().unwrap();
+
+    loop {
+        timer.delay_ms(200);
+        pins.col1.set_high().unwrap();
+        timer.delay_ms(200);
+        pins.col1.set_low().unwrap();
+    }
+}
