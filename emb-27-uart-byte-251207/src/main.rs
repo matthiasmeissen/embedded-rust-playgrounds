5874bdd0c54afb93d9f45415c434d47841fc8d56
matthiasmeissen <meissenmatthias@gmail.com>
2025-12-07 18:41:59 +0100

    Add Project 27

---
+++ emb-27-uart-byte-251207/src/main.rs
@@ -0,0 +1,42 @@
+
+#![no_std]
+#![no_main]
+
+use cortex_m::asm::wfi;
+use cortex_m_rt::entry;
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+use microbit::{
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+    board,
+};
+
+mod serial_setup;
+
+use serial_setup::UartePort;
+
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = board::Board::take().unwrap();
+
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    serial.write(b'X').unwrap();
+    serial.flush().unwrap();
+
+    loop {
+        wfi();
+    }
+}
