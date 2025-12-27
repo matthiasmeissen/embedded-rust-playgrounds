2012f524edf73ed314e8abe2e992577429895ea0
matthiasmeissen <meissenmatthias@gmail.com>
2025-12-27 19:26:16 +0100

    Finish Mag Basics

--- emb-41-mag-basics-251227/src/main.rs
+++ emb-41-mag-basics-251227/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use microbit::{
+    board, 
+    hal::{Timer, twim}, 
+    pac::twim0::{frequency::FREQUENCY_A},
+};
+use lsm303agr::{MagMode, MagOutputDataRate, Lsm303agr};
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = board::Board::take().unwrap();
+    let i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+    let mut timer = Timer::new(board.TIMER0);
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_mag_mode_and_odr(&mut timer, MagMode::HighResolution, MagOutputDataRate::Hz10).unwrap();
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
+
+    rprintln!("Mag init done.");
+
+    loop {
+        if sensor.mag_status().unwrap().xyz_new_data() {
+            let (x, y, z) = sensor.magnetic_field().unwrap().xyz_nt();
+
+            rprintln!("X: {}, Y: {}, Z: {}", x, y, z);
+        }
+    };
 }
