14d2120a27819c9a6f07773d8861b9de88830b88
matthiasmeissen <meissenmatthias@gmail.com>
2025-12-28 21:11:09 +0100

    Add Project 42

---
+++ emb-42-mag-field-251228/src/main.rs
@@ -0,0 +1,17 @@
+
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use microbit::{
+    board, 
+    hal::{Timer, twim}, 
+    pac::twim0::{frequency::FREQUENCY_A},
+};
+use lsm303agr::{MagMode, MagOutputDataRate, Lsm303agr};
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+
+fn main() {
+    println!("Hello, world!");
+}
