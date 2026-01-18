119050a42c88603ba6b69325d2f9aa7b0f88a7cb
matthiasmeissen <meissenmatthias@gmail.com>
2026-01-18 17:50:03 +0100

    Add Project 56

---
+++ emb-56-audio-basics-260117/src/main.rs
@@ -0,0 +1,20 @@
+
+#![no_std]
+#![no_main]
+
+use core::{cell::RefCell, sync::atomic::AtomicUsize};
+
+use cortex_m::{asm, interrupt::Mutex};
+use cortex_m_rt::entry;
+use microbit::{
+    Board, 
+    hal::{Timer, gpiote::Gpiote, pac::interrupt}, 
+    pac::{self, TIMER0}
+};
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+#[entry]
+fn main() -> ! {
+    loop {}
+}
