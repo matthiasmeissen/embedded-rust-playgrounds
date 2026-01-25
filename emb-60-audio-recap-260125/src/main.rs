2c43478a754b523407b2d05eaf51ba501eb3b3ab
matthiasmeissen <meissenmatthias@gmail.com>
2026-01-25 14:52:43 +0100

    Add Project 60

---
+++ emb-60-audio-recap-260125/src/main.rs
@@ -0,0 +1,19 @@
+
+#![no_std]
+#![no_main]
+
+use core::{cell::RefCell, sync::atomic::AtomicUsize};
+
+use cortex_m::{asm, interrupt::Mutex};
+use cortex_m_rt::entry;
+use embedded_hal::{digital::OutputPin, delay::DelayNs};
+use microbit::{
+    Board, hal::{Timer, gpiote::Gpiote, pac::interrupt}, pac::{self, TIMER0, rtc0::COUNTER}
+};
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+#[entry]
+fn main() -> ! {
+    loop {}
+}
