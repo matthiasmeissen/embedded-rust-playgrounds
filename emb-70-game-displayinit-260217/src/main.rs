8e7e35f3d2736c835f5dc609aa2ff535bc91badd
matthiasmeissen <meissenmatthias@gmail.com>
2026-02-17 21:06:52 +0100

    Add Project 70

---
+++ emb-70-game-displayinit-260217/src/main.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use microbit::{
+    board, display::nonblocking::{Display, GreyscaleImage}, hal::{Rng, Timer, clocks::Clocks}
+};
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+mod game;
+use game::rng::Prng;
+use game::snake::Snake;
+
+mod controls;
+mod display;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = board::Board::take().unwrap();
+
+    display::init_display(board.TIMER1, board.display_pins, board.CLOCK, board.RTC0);
+
+    controls::init_buttons(board.GPIOTE, board.buttons);
+
+    let mut hardware_rng = Rng::new(board.RNG);
+    let mut prng = Prng::seeded(&mut hardware_rng);
+    let random_number = prng.random_u32();
+    rprintln!("Random number is: {}", random_number);
+
+    let snake = Snake::new();
+    rprintln!("Init             Snake is: {:?}", snake);
+
+
+    loop {
+        asm::wfi();
+
+        let current_turn = controls::get_turn(false);
+        rprintln!("Turn is: {:?}", current_turn);
+    }
+}
